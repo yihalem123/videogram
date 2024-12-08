52b9ed4aa9a14adf4884ee922c6e90e555416049
Yihalem Mandefro <70970259+yihalem123@users.noreply.github.com>
2024-12-08 14:11:31 +0300

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,33 +1,33 @@
 import requests
 from django.conf import settings
 from .models import User
-from bip32utils import BIP32Key
+from bitcoinlib.keys import HDKey
 
 def generate_btc_address_for_user(user_id):
-    """Generate a unique Bitcoin address for a user."""
     try:
-        xpub = settings.XPUB
-        master_key = BIP32Key.fromExtendedKey(xpub)
+        zpub = settings.ZPUB
+        master_key = HDKey(zpub)
 
         if isinstance(user_id, int):
-            user_id = str(user_id)  # Convert to string if it's an integer
+            user_id = str(user_id)
 
         index = int(user_id) if user_id.isdigit() else int.from_bytes(user_id.encode(), 'big') % (2**31)
-        child_key = master_key.ChildKey(0).ChildKey(index)
-        return child_key.Address()
+
+        child_key = master_key.subkey_for_path(f"m/84h/0h/0h/0/{index}")
+
+        return child_key.address(witness_type='p2wpkh')
     except ValueError:
         raise ValueError(f"Invalid user_id: {user_id}")
     except Exception as e:
         raise Exception(f"Error generating BTC address: {str(e)}")
 
 def check_payments():
-    """Check payments for non-premium users and update their status if the payment is received."""
     rpc_url = settings.BITCOIN_RPC_URL
     rpc_user = settings.BITCOIN_RPC_USER
     rpc_pass = settings.BITCOIN_RPC_PASS
 
     headers = {'content-type': 'text/plain;'}
-    threshold = 50000  # 0.0005 BTC in Satoshis
+    threshold = 50000
 
     for user in User.objects.filter(is_premium=False, btc_address__isnull=False):
         payload = {
@@ -39,7 +39,7 @@ def check_payments():
             response = requests.post(rpc_url, json=payload, auth=(rpc_user, rpc_pass), headers=headers)
             response.raise_for_status()
             result = response.json().get('result', 0)
-            if result * 1e8 >= threshold:  # Convert BTC to Satoshis for comparison
+            if result * 1e8 >= threshold:
                 user.is_premium = True
                 user.save()
         except Exception as e:
